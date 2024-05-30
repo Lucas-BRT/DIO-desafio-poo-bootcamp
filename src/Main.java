a27174d1c1179922e8b111c4593839a7f4d27568
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-29 22:19:14 -0300

    testando criaçao de curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso = new Curso();
+        System.out.println(curso);
+        curso.setTitulo("curso java");
+        curso.setDescricao("curso curto de java");
+        curso.setCargaHoraria(8);
+        System.out.println(curso);
 
     }
 }
