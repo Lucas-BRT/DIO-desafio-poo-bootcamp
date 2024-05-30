61fb47bea47fde3b166d90f7ceea6f57db077705
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-29 22:14:40 -0300

    adiciona geters and seters e estrutura básica à mentoria e curso

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,4 +1,42 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+
 public class Curso {
+    private String titulo;
+    private String descricao;
+    private LocalDate cargaHoraria;
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(LocalDate cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 }
